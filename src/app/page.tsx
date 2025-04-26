c8bc039b9427b8e0614ca582fe38c41594a0bed7
Praise Odhiambo <87634669+godwins3@users.noreply.github.com>
2025-04-26 15:34:37 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,7 +42,7 @@ const featuredDestinations = [
 const whyUsFeatures = [
   {
     title: 'Unique Destinations',
-    description: 'We offer tours to off-the-beaten-path locations that you won\&apos;t find with other agencies.',
+    description: 'We offer tours to off-the-beaten-path locations that you won\'t find with other agencies.',
     icon: (
       <svg className="w-8 h-8 text-emerald-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
@@ -69,7 +69,7 @@ const whyUsFeatures = [
   },
   {
     title: 'World Class Service',
-    description: 'We offer customer experiences that you won\&apos;t find with other agencies.',
+    description: 'We offer customer experiences that you won\'t find with other agencies.',
     icon: (
       <svg className="w-8 h-8 text-emerald-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"></path>
